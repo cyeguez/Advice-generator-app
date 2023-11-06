87b119bca73ec55bc7c5b32d1b68e2397ca9568f
cyeguez <cesar.yeguez@gmail.com>
2023-11-06 15:49:54 -0300

    feat(Comentar el código js)

--- js/app.js
+++ js/app.js
@@ -1,19 +1,19 @@
-const url = "https://api.adviceslip.com/advice";
-const number = document.querySelector(".container__title--number");
-const btn = document.querySelector(".container__circle");
-const advice = document.querySelector(".container__text");
+const url = "https://api.adviceslip.com/advice"; //ruta de la api
+const number = document.querySelector(".container__title--number"); //ubicacion del id en el html
+const btn = document.querySelector(".container__circle");// el bon en el html
+const advice = document.querySelector(".container__text");//ubicación del consejo en el html
 
-function show(x, y) {
-  number.innerHTML = `#${x}`;
-  advice.innerHTML = `"${y}"`;
+function show(x, y) { //funcion que pinta en el DOM
+  number.innerHTML = `#${x}`; //Pintal el id del consejo con el #
+  advice.innerHTML = `"${y}"`; //pinta el consejo co las comillas
 }
 
 
-btn.addEventListener("click", () => {
+btn.addEventListener("click", () => { //Colocando el evento al boton actualiza el consejo
   
-  fetch(url, {cache: 'no-store'})
-    .then((response) => response.json())
+  fetch(url, {cache: 'no-store'})// Haciendo la peticion co el fech
+    .then((response) => response.json()) //transformando la respuesta a un json
     .then((data) => {
-      show(data.slip.id, data.slip.advice);
+      show(data.slip.id, data.slip.advice);// pasandole los argumentos a la funcion show 
     });
 });
