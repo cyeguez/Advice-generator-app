129ee5968d1f84b4799db6750416efacdc81e4cd
cyeguez <cesar.yeguez@gmail.com>
2023-11-06 15:42:11 -0300

    feat(Culminar parte lógica del proyecto en js)

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 const url = "https://api.adviceslip.com/advice";
-const container = document.querySelector(".container");
 const number = document.querySelector(".container__title--number");
 const btn = document.querySelector(".container__circle");
 const advice = document.querySelector(".container__text");
@@ -8,16 +7,11 @@ function show(x, y) {
   number.innerHTML = `#${x}`;
   advice.innerHTML = `"${y}"`;
 }
-fetch(url)
-  .then((response) => response.json())
-  .then((data) => {
-    show(data.slip.id, data.slip.advice);
-  });
+
 
 btn.addEventListener("click", () => {
-  // show("", "");
-
-  fetch(url)
+  
+  fetch(url, {cache: 'no-store'})
     .then((response) => response.json())
     .then((data) => {
       show(data.slip.id, data.slip.advice);
